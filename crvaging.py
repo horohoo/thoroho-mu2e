3578be1d8b750c2a83b87c1ca71062aadea230af
tylerhoroho-UVA <tgh7hx@virginia.edu>
2022-07-08 11:17:22 -0400

    Finished product v1

--- crvaging.py
+++ crvaging.py
@@ -4,7 +4,7 @@
 # each run, as the script has hard-coded info about the dates and     #
 # number of runs. It is critical to import files in chronological     #
 # order (i.e. the order in which the runs happened                    #
-# Created on Thu 7 July 2022                                         #
+# Created on Thu 7 July 2022                                          #
 # @author: Tyler Horoho                                               #
 #######################################################################
 
@@ -22,15 +22,50 @@ if (nfiles == 0):
 
 else:
     # import the data
-    for i in range(1, nfiles + 1):
-        d["data_{0}".format(i)] = np.genfromtxt(str(sys.argv[i]), skip_header=2)
+    date = np.zeros(nfiles)
+    PE_yield = np.zeros(nfiles)
+    for i in range(nfiles):
+        d["data_{0}".format(i)] = np.genfromtxt(str(sys.argv[i + 1]), skip_header=2)
         
         # hard-coded information about dates of runs
-        # an example of the first run (66)
-        if i <= 2:
-            d["date_{0}".format(i)] = datetime.datetime(2021, 7, 8)
-        # do this for the other dates and check the number of files
+        if i <= 1:
+            date[i] = datetime.datetime(2021, 7, 8)
+        if i >= 2 and i <= 14:
+            date[i] = datetime.datetime(2021, 12, 10)
+        if i >= 15 and i <= 25:
+            date[i] = datetime.datetime(2022, 1, 12)
+        if i == 26:
+            date[i] = datetime.datetime(2022, 2, 15)
+        if i == 27:
+            date[i] = datetime.datetime(2022, 2, 25)
+        if i == 28:
+            date[i] = datetime.datetime(2022, 3, 7)
+        if i == 29:
+            date[i] = datetime.datetime(2022, 3, 10)
+        if i >= 30 and i <= 32:
+            date[i] = datetime.datetime(2022, 3, 18)
+        if i == 33:
+            date[i] = datetime.datetime(2022, 4, 27)
+        if i == 34 or i == 35:
+            date[i] = datetime.datetime(2022, 4, 29)
+        if i >= 36 and i <= 40:
+            date[i] = datetime.datetime(2022, 5, 2)
+        if i == 41:
+            date[i] = datetime.datetime(2022, 5, 11)
 
     # now that the data is imported, loop through each FEB/channel and plot data
     for j in range(128): # 0-63 are FEB 0 and 64-127 are FEB 1
-        np.append(PE_yield, 
+        for i in range(nfiles):
+            data = d["data_{0}".format(i)]
+            PE_yield[i] = data[j, 3]
+        if j < 64:
+            feb = 0
+            channel = j
+        else:
+            feb = 1
+            channel = j - 64
+        plt.scatter(date, PE_yield)
+        plt.xlabel('Date of run')
+        plt.ylabel('PE yield')
+        plt.title('PE yield over time for FEB {0}, channel {1}'.format(feb, channel))
+        plt.savefig('aging_feb{0}_ch{1}.pdf'.format(feb, channel))
